6254f47590cb1aa878dc38363bbbf5c675617822
taehwan1-6 <47453097+taehwan1-6@users.noreply.github.com>
2023-04-23 01:44:54 +0900

    feat: BookRepository 생성, BookRepoDbImpl 생성

---
+++ src/com/kopo/library/repository/BookRepoDbImpl.java
@@ -0,0 +1,42 @@
+package com.kopo.library.repository;
+
+import com.kopo.library.domain.Book;
+
+import java.util.List;
+
+public class BookRepoDbImpl implements BookRepository{
+    @Override
+    public void insertBook(Book book) {
+
+    }
+
+    @Override
+    public void updateBook(Book book) {
+
+    }
+
+    @Override
+    public void deleteBook(Book book) {
+
+    }
+
+    @Override
+    public List<Book> findAllBook() {
+        return null;
+    }
+
+    @Override
+    public Book findById(Long bookId) {
+        return null;
+    }
+
+    @Override
+    public Book findByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public Book restore(Book book) {
+        return null;
+    }
+}
---
+++ src/com/kopo/library/repository/BookRepository.java
@@ -0,0 +1,28 @@
+package com.kopo.library.repository;
+
+import com.kopo.library.domain.Book;
+import com.kopo.library.domain.Member;
+
+import java.util.List;
+
+public interface BookRepository {
+    // CRUD
+    // 삽입
+    void insertBook(Book book);
+
+    // 수정
+    void updateBook(Book book);
+
+    // 삭제
+    void deleteBook(Book book);
+
+
+    // 조회
+    List<Book> findAllBook();
+
+    Book findById(Long bookId);
+
+    Book findByTitle(String title);
+
+    Book restore(Book book);
+}
