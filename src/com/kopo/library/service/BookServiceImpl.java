008ff1d833cf49371742a7e26cbea04cedc30dcd
taehwan1-6 <47453097+taehwan1-6@users.noreply.github.com>
2023-04-23 00:20:54 +0900

    feat: CrudService 생성, BookServiceImpl 생성 및 조회기능 추가

---
+++ src/com/kopo/library/service/BookServiceImpl.java
@@ -0,0 +1,51 @@
+package com.kopo.library.service;
+
+import com.kopo.library.repository.CrudRepository;
+import com.kopo.library.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Objects;
+
+public class BookServiceImpl implements CrudService{
+
+    private CrudRepository bookRepository;
+
+    public BookServiceImpl(CrudRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void insertObjects(Objects objects) {
+
+    }
+
+    @Override
+    public void updateObjects(Objects objects) {
+
+    }
+
+    @Override
+    public void deleteObjects(Objects objects) {
+
+    }
+
+    @Override
+    public List findAllObjects() {
+        return bookRepository.findAllObjects();
+    }
+
+    @Override
+    public Objects findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Objects findByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public Objects restore(Objects objects) {
+        return null;
+    }
+}
---
+++ src/com/kopo/library/service/CrudService.java
@@ -0,0 +1,28 @@
+package com.kopo.library.service;
+
+import com.kopo.library.domain.;
+
+import java.util.List;
+import java.util.Objects;
+
+public interface CrudService<T> {
+    // CRUD
+    // 삽입
+    void insertObjects(Objects objects);
+
+    // 수정
+    void updateObjects(Objects objects);
+
+    // 삭제
+    void deleteObjects(Objects objects);
+
+
+    // 조회
+    List<T> findAllObjects();
+
+    Objects findById(Long id);
+
+    Objects findByTitle(String title);
+
+    Objects restore(Objects objects);
+}
