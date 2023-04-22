75ea1b2b5d727062a7ae31fc022c14abbc1e7c0e
taehwan1-6 <47453097+taehwan1-6@users.noreply.github.com>
2023-04-23 00:16:31 +0900

    feat: BookRepoDbImpl 조회 기능 추가

--- src/com/kopo/library/repository/BookRepoDbImpl.java
+++ src/com/kopo/library/repository/BookRepoDbImpl.java
@@ -1,11 +1,23 @@
 package com.kopo.library.repository;
 
 import com.kopo.library.domain.Book;
+import com.kopo.library.domain.GenderStatus;
+import com.kopo.library.domain.Member;
+import com.kopo.library.view.MainView;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class BookRepoDbImpl implements CrudRepository{
+
+    Connection connection = MainView.connection;
+//    Book book = MainView.book;
+    List<Book> books = MainView.books;
 
     @Override
     public void insertObjects(Objects objects) {
@@ -23,8 +35,32 @@ public class BookRepoDbImpl implements CrudRepository{
     }
 
     @Override
-    public List<Objects> findAllObjects() {
-        return null;
+    public List<Book> findAllObjects() {
+
+        String query = "SELECT * FROM BOOK "
+                + "ORDER BY ID";
+
+        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(query);)
+        {
+            // Query 결과를 처리
+            while (resultSet.next()) {
+                Long bookId = resultSet.getLong("bookId");
+                String title = resultSet.getString("title");
+                String author = resultSet.getString("author");
+                String publisher = resultSet.getString("publisher");
+                String publicationDate = resultSet.getString("publicationDate");
+                boolean isPossibleBorrow = resultSet.getBoolean("isPossibleBorrow");
+
+                Book book = new Book(bookId, title, author, publisher, publicationDate, isPossibleBorrow);
+                books.add(book);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("SQL Statement or DB Connection Error Occur");
+            e.printStackTrace();
+        }
+
+        return books;
     }
 
     @Override
