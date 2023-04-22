16734730bce5ca1a517090e4cc165d203b9617bb
taehwan1-6 <47453097+taehwan1-6@users.noreply.github.com>
2023-04-23 01:54:32 +0900

    feat: BookRepoDbImpl 조회 기능 추가

--- src/com/kopo/library/repository/BookRepoDbImpl.java
+++ src/com/kopo/library/repository/BookRepoDbImpl.java
@@ -1,10 +1,22 @@
 package com.kopo.library.repository;
 
 import com.kopo.library.domain.Book;
+import com.kopo.library.view.MainView;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookRepoDbImpl implements BookRepository{
+
+    Connection connection = MainView.connection;
+    //    Book book = MainView.book;
+//    List<Book> books = MainView.books;
+
+
     @Override
     public void insertBook(Book book) {
 
@@ -22,7 +34,33 @@ public class BookRepoDbImpl implements BookRepository{
 
     @Override
     public List<Book> findAllBook() {
-        return null;
+        List<Book> books = new ArrayList<>();
+
+        String query = "SELECT * FROM BOOK "
+                + "ORDER BY ID";
+
+        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(query);)
+        {
+            // Query 결과를 처리
+            while (resultSet.next()) {
+                Long bookId = resultSet.getLong("bookId");
+                String title = resultSet.getString("title");
+                String author = resultSet.getString("author");
+                String publisher = resultSet.getString("publisher");
+                String publicationDate = resultSet.getString("publicationDate");
+                boolean isPossibleBorrow = resultSet.getBoolean("isPossibleBorrow");
+
+                Book book = new Book(bookId, title, author, publisher, publicationDate, isPossibleBorrow);
+                books.add(book);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("SQL Statement or DB Connection Error Occur");
+            e.printStackTrace();
+        }
+
+        return books;
+
     }
 
     @Override
